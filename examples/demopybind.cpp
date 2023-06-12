da5414dfcd6483f1fe7240615cb326e78be0e0f3
snehpreet <dhinsasnehpreet@gmail.com>
2023-06-12 13:14:43 +0200

    l

--- examples/demopybind.cpp
+++ examples/demopybind.cpp
@@ -45,63 +45,7 @@ PYBIND11_MODULE(schiffeversenken, m) {
         
 
     py::class_<::Game>(m, "Game");
-        .def("printRules", &Game::printRules, "Prints the rules");
+        .def("printRules", &Game::printRules, "Prints the rules");   
     
-    
-}
+} 
 
-
-/* 
-
-server phyton: 
-
-import socket
-
-def run_game():
-    # Implementiere hier den Spielablauf
-
-def start_server():
-    host = '127.0.0.1'
-    port = 12345
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(1)
-
-    print(f"Server gestartet. Warte auf Verbindung auf {host}:{port}...")
-
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Verbindung hergestellt von {addr[0]}:{addr[1]}")
-
-        # Führe das Spiel aus
-        run_game()
-
-        client_socket.close()
-
-if __name__ == '__main__':
-    start_server()
-
-
-
-client Phyton: 
-
-import socket
-
-def start_client():
-    host = '127.0.0.1'
-    port = 12345
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-
-    print(f"Verbindung hergestellt zu {host}:{port}")
-
-    # Hier können Sie die Logik für den Client implementieren
-    # z.B. Anweisungen an den Server senden und die Antworten empfangen
-
-    client_socket.close()
-
-if __name__ == '__main__':
-    start_client()
-*/ 
